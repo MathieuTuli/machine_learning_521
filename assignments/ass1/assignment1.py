c164760ca92907f957bad91f4e8bfd76fda92e2d
Mathieu Tuli <tuli.mathieu@gmail.com>
2018-01-29 19:48:12 -0500

    almost done part 3

--- assignments/ass1/assignment1.py
+++ assignments/ass1/assignment1.py
@@ -156,29 +156,110 @@ def solve_KNN():
 
     return
 
+
+#----------Question: 3---------------------------------------------------------
+
+def data_segmentation(data_path, target_path, task):
+    # task = 0 >> select the name ID targets for face recognition task
+    # task = 1 >> select the gender ID targets for gender recognition task
+    data = np.load(data_path)/255
+    data = np.reshape(data, [-1, 32*32])
+    target = np.load(target_path)
+
+    np.random.seed(45689)
+    rnd_idx = np.arange(np.shape(data)[0])
+    np.random.shuffle(rnd_idx)
+
+    trBatch = int(0.8*len(rnd_idx))
+    validBatch = int(0.1*len(rnd_idx))
+
+    trainData, validData, testData = data[rnd_idx[1:trBatch],:], \
+                                    data[rnd_idx[trBatch+1:trBatch + validBatch],:],\
+                                    data[rnd_idx[trBatch + validBatch+1:-1],:]
+
+    trainTarget, validTarget, testTarget = target[rnd_idx[1:trBatch], task], \
+                                            target[rnd_idx[trBatch+1:trBatch + validBatch], task],\
+                                            target[rnd_idx[trBatch + validBatch + 1:-1], task]
+
+    return trainData, validData, testData, trainTarget, validTarget, testTarget
+
+def classification_prediction(trainData, trainTarget, sampleData, sampleTarget, K):
+    #compute the euclidean distance between test and training
+    testDistance = eucl_dist(sampleData, trainData)
+
+    #defined to hold the majority class from each row of the image
+    majority = []
+
+    #for each row, find the k nearest neighbours, and determine which class
+    #this row most seems to resemble. aggregate the results for each row and
+    #at the very end, classify based on the majority class that showed up
+    #in the aggregate result
+    print(tf.shape(testDistance).to_list()) #.as_list()[0])
+    # for i in range(testDistance.get_shape().as_list()[0]):
+    #     #iteratively grab a new row
+    #     currentRow = tf.gather(testDistance, i)
+    #
+    #     #find the k nearest neighbours (and their indices) in each row, where a row
+    #     #represents a different data point. Take the -ve of the matrix since the
+    #     #smaller the value the closer it is, bigger +ve numbers become smaller -ve
+    #     #numbers
+    #     neighbours, neighboursIndices = tf.nn.top_k(-currentRow,k = K)
+    #
+    #     #from nereast neighbours, gather the corresponding rows
+    #     possibleClassificationsVec = tf.gather(trainTarget,neighboursIndices)
+    #
+    #     #run unique_with_counts to find majority classification
+    #     y, idx, count = tf.unique_with_counts(possibleClassificationsVec)
+    #
+    #     #append the majority class from the current row
+    #     majority.append(tf.argmax(count))
+
+    #return maajority which has predictions of each image per row
+    return majority
+
+def classify():
+    #define our placeholders
+    K = tf.placeholder(tf.int32, name = "K")
+    trainX = tf.placeholder(tf.float32, name = "trainX")
+    trainY = tf.placeholder(tf.float32, name = "trainY")
+    newX = tf.placeholder(tf.float32, name = "newX")
+    newY = tf.placeholder(tf.float32, name = "newY")
+
+    #define possible Ks
+    possibleK = [1,5,10,15,25,50,100,200]
+
+    #load data
+    #run for name ID. task = 0
+    trainData0, validData0, testData0, trainTarget0, validTarget0, testTarget0 = \
+        data_segmentation("./data.npy", "./target.npy", 0)
+
+    # print(sess.run(tf.shape(trainTarget)),sess.run(tf.shape(validTarget)),sess.run(tf.shape(testTarget)))
+    #run for gender ID. task = 1
+    trainData1, validData1, testData1, trainTarget1, validTarget1, testTarget1 = \
+        data_segmentation("./data.npy", "./target.npy", 1)
+
+    #compute the MSE loss for each possible k
+    trainingError = []
+    validationError = []
+    testError = []
+
+    for currK in range(1):
+        classifications = []
+        classifications.append(sess.run(classification_prediction(trainX, \
+        trainY, newX, newY, K), feed_dict={trainX:trainData0, trainY:trainTarget0, \
+        newX:validData0, newY:validTarget0, K:currK}))
+
+        print('\n\nclass: ',classifications)
+
+    return
+
 if __name__ == '__main__':
     #serves no other purpose other than to provide spacing from cpu compilation
     #suggestion messages that pop up
     print('\n\n\n\n\n---------Assignment 1: KNN Regression---------\n\n')
 
-    #run the program
+    #part 1
     solve_KNN()
 
-#----------Question: 3---------------------------------------------------------
-#
-# def data_segmentation(data_path, target_path, task):
-#     # task = 0 >> select the name ID targets for face recognition task
-#     # task = 1 >> select the gender ID targets for gender recognition task
-#     data = np.load(data_path)/255
-#     data = np.reshape(data, [-1, 32*32])
-#     target = np.load(target_path)
-#     np.random.seed(45689) rnd_idx = np.arange(np.shape(data)[0]) np.random.shuffle(rnd_idx)
-#     trBatch = int(0.8*len(rnd_idx)) validBatch = int(0.1*len(rnd_idx))
-#     trainData, validData, testData = data[rnd_idx[1:trBatch],:], \
-#                                     data[rnd_idx[trBatch+1:trBatch + validBatch],:],\
-#                                     data[rnd_idx[trBatch + validBatch+1:-1],:]
-#     trainTarget, validTarget, testTarget = target[rnd_idx[1:trBatch], task], \
-#                                             target[rnd_idx[trBatch+1:trBatch + validBatch], task],\
-#                                             target[rnd_idx[trBatch + validBatch + 1:-1], task]
-#
-#     return trainData, validData, testData, trainTarget, validTarget, testTarget
+    #part 3
+    classify()
