5170ddbb076265cdff9afe794af0cb052721a3e7
Mathieu Tuli <tuli.mathieu@gmail.com>
2018-03-14 18:08:13 -0400

    transfer to pc

--- assignments/ass2/assignment2_all_parts.py
+++ assignments/ass2/assignment2_all_parts.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 sess = tf.Session()
+PIXELCOUNT = 28*28
 
 def load_data():
     with np.load("notMNIST.npz") as data :
@@ -28,9 +29,9 @@ def load_data():
         testData, testTarget = Data[3600:], Target[3600:]
 
         #shuffle and reshape data
-        trainData = np.reshape(trainData, (3500,-1))
-        validData = np.reshape(validData, (100,-1))
-        testData = np.reshape(testData, (145,-1))
+        trainData = np.reshape(trainData, (trainData.shape[0],-1))
+        validData = np.reshape(validData, (validData.shape[0],-1))
+        testData = np.reshape(testData, (testData.shape[0],-1))
         return trainData, trainTarget, validData, validTarget, testData, testTarget
 
 def calculateCrossEntropyLoss(y, yHat, W, wdc):
@@ -49,23 +50,40 @@ def calculateCrossEntropyLoss(y, yHat, W, wdc):
 def prediction(X, W, b):
     return tf.matmul(X, W) + b
 
-def _1_1():
-    B = tf.placeholder(tf.int32, name = "B")
-    trainX = tf.placeholder(tf.float32, name = "trainX")
-    trainY = tf.placeholder(tf.float32, name = "trainY")
-    newX = tf.placeholder(tf.float32, name = "newX")
-    newY = tf.placeholder(tf.float32, name = "newY")
+def _1():
+    #define placeholders
+    trainX = tf.placeholder(tf.float64, shape = [None, PIXELCOUNT], name = "trainData")
+    trainY = tf.placeholder(tf.float64, shape = [None, 1], name = "trainTarget")
+    validationX = tf.placeholder(tf.float64, shape = [None, PIXELCOUNT], name = "validationData")
+    validationY = tf.placeholder(tf.float64, shape = [None, 1], name = "validationTarget")
+    testX = tf.placeholder(tf.float64, name="testData")
+    testY = tf.placeholder(tf.float64, name = "testTarget")
+    b = tf.Variable(tf.truncated_normal(shape = [1], stddev = 0.1, dtype = tf.float64, name = "biases"))
+    W = tf.Variable(tf.truncated_normal([PIXELCOUNT, 1], stddev = 0.5, dtype = tf.float64, name = "weights"))
 
-    SGD = tf.train.GradientDescentOptimizer
+    #Initialize
+    sess.run(tf.global_variables_initializer())
+
+    #other variables
     trainData, trainTarget, validData, validTarget, testData, testTarget = load_data()
+
+    numTrainingSamples = trainData.shape[0]
+    numValidationSamples = validData.shape[0]
+    numTestSamples = testData.shape[0]
+
     possibleB = [500, 1500, 3500]
-    possibleLambda = [0., 0.001, 0.1, 1]
-    iterations = 2000
+    possibleWdc = [0., 0.001, 0.1, 1]
+    iterations = 20000
     learning_rates = [0.005, 0.001, 0.0001]
+
+    indices = np.arange(0, numTrainingSamples)
+
+    # optimizer = tf.train.GradientDescentOptimizer(learning_rate = learningRate)
+
     return
 
 if __name__ == '__main__':
     #serves no other purpose other than to provide spacing from cpu compilation
     #suggestion messages that pop up
     print('\n\n\n---------Assignment 1---------\n\n')
-    linear_regression()
+    _1()
